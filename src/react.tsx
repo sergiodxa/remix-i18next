a95028164a9a9e81377bd18c40340256ff7e1ca9
Sergio Xalambrí <hello@sergiodxa.com>
2022-03-25 19:56:58 -0500

    Add useNamespace hook and use the useSSR hook

--- src/react.tsx
+++ src/react.tsx
@@ -1,6 +1,5 @@
 import { useMatches } from "@remix-run/react";
-import { useCallback, useEffect, useMemo } from "react";
-import { useTranslation } from "react-i18next";
+import { useSSR } from "react-i18next";
 import useConsistentValue from "use-consistent-value";
 import { Language } from "./backend";
 
@@ -10,32 +9,26 @@ import { Language } from "./backend";
  * @param locale The locale to use.
  */
 export function useSetupTranslations(locale: string) {
+  let namespaces = useNamespaces(locale);
+  useSSR(namespaces, locale);
+}
+
+/**
+ * Get the namespaces from the loaders data.
+ * Returns them as expected by useSSR from React i18next.
+ * @param locale The locale to use.
+ */
+export function useNamespaces(locale: string) {
   if (!locale) throw new Error("Missing locale");
 
-  let { i18n } = useTranslation();
-
-  let namespaces = useConsistentValue(
-    useMatches()
-      .flatMap((match) => (match.data?.i18n ?? {}) as Record<string, Language>)
-      // eslint-disable-next-line unicorn/no-array-reduce
-      .reduce(
-        (messages, routeMessages) => ({ ...messages, ...routeMessages }),
-        {}
-      )
+  let matches = useMatches();
+  let data = matches.map(
+    (match) => (match.data?.i18n ?? {}) as Record<string, Language>
   );
+  let namespaces: Record<string, Language> = {};
+  for (let item of data) {
+    namespaces = { ...namespaces, ...item };
+  }
 
-  let handleLocaleUpdate = useCallback(() => {
-    void i18n.changeLanguage(locale);
-    for (let [namespace, messages] of Object.entries(namespaces)) {
-      i18n.addResourceBundle(locale, namespace, messages);
-    }
-  }, [i18n, namespaces, locale]);
-
-  useMemo(() => {
-    handleLocaleUpdate();
-  }, []);
-
-  useEffect(() => {
-    handleLocaleUpdate();
-  }, [handleLocaleUpdate]);
+  return useConsistentValue({ [locale]: namespaces });
 }
