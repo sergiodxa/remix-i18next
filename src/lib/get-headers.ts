f3f6c50c867faf7cc169d2ff289416edb830b654
erlanbelekov <belekoverlanhehe@gmail.com>
2023-02-16 18:54:25 +0600

    fix getHeaders

--- src/lib/get-headers.ts
+++ src/lib/get-headers.ts
@@ -1,12 +1,12 @@
 /**
  * Receives a Request or Headers objects.
- * If it's a Request returns the request.headers
- * If it's a Headers returns the object directly.
+ * If it's a Request returns the object directly
+ * If it's a Headers returns the request.headers
  */
 export function getHeaders(requestOrHeaders: Request | Headers): Headers {
-  if (requestOrHeaders instanceof Request) {
-    return requestOrHeaders.headers;
+  if (requestOrHeaders instanceof Headers) {
+    return requestOrHeaders;
   }
 
-  return requestOrHeaders;
+  return requestOrHeaders.headers;
 }
