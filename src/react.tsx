a720631f0446ecf7697810bf779ab427844a5f87
Sergio Xalambrí <hello@sergiodxa.com>
2024-08-05 14:50:58 -0500

    Remove undocumented PreloadTranslations (#200)
    This component was never documented but TypeDoc will try to document it,
    since it's not considered ready to use I'm removing it

--- src/react.tsx
+++ src/react.tsx
@@ -2,54 +2,6 @@ import { useMatches } from "@remix-run/react";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 
-export interface PreloadTranslationsProps {
-	loadPath: string;
-}
-
-/**
- * Preload the translations files for the current language and the namespaces
- * required by the routes.
- *
- * It receives a single `loadPath` prop with the path to the translation files.
- *
- * @example
- * <PreloadTranslations loadPath="/locales/{{lng}}/{{ns}}.json" />
- *
- */
-export function PreloadTranslations({ loadPath }: PreloadTranslationsProps) {
-	let { i18n } = useTranslation();
-
-	let namespaces = [
-		...new Set(
-			useMatches()
-				.filter(
-					(route) =>
-						(route.handle as { i18n?: string | string[] })?.i18n !== undefined,
-				)
-				.flatMap((route) => (route.handle as { i18n: string | string[] }).i18n),
-		),
-	];
-
-	let lang = i18n.language;
-
-	return (
-		<>
-			{namespaces.map((namespace) => {
-				return (
-					<link
-						key={namespace}
-						rel="preload"
-						as="fetch"
-						href={loadPath
-							.replace("{{lng}}", lang)
-							.replace("{{ns}}", namespace)}
-					/>
-				);
-			})}
-		</>
-	);
-}
-
 /**
  * Get the locale returned by the root route loader under the `locale` key.
  * @example
