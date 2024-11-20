036a5b2f23b11cb8cc05ecacd44e501793a77ae2
Alem Tuzlak <t.zlak@hotmail.com>
2024-11-20 17:54:38 +0100

    fix for linting

--- src/client.ts
+++ src/client.ts
@@ -14,21 +14,21 @@ declare global {
  * })
  */
 export function getInitialNamespaces(): string[] {
-	let namespaces = Object.values(
-		window.__reactRouterRouteModules,
-	).flatMap((route) => {
-		if (typeof route?.handle !== "object") return [];
-		if (!route.handle) return [];
-		if (!("i18n" in route.handle)) return [];
-		if (typeof route.handle.i18n === "string") return [route.handle.i18n];
-		if (
-			Array.isArray(route.handle.i18n) &&
-			route.handle.i18n.every((value: unknown) => typeof value === "string")
-		) {
-			return route.handle.i18n as string[];
-		}
-		return [];
-	});
+	let namespaces = Object.values(window.__reactRouterRouteModules).flatMap(
+		(route) => {
+			if (typeof route?.handle !== "object") return [];
+			if (!route.handle) return [];
+			if (!("i18n" in route.handle)) return [];
+			if (typeof route.handle.i18n === "string") return [route.handle.i18n];
+			if (
+				Array.isArray(route.handle.i18n) &&
+				route.handle.i18n.every((value: unknown) => typeof value === "string")
+			) {
+				return route.handle.i18n as string[];
+			}
+			return [];
+		},
+	);
 
 	return [...namespaces];
 }
