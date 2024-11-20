f268f0fc77be0968504bd6b8a1be7bb23ee24184
Alem Tuzlak <t.zlak@hotmail.com>
2024-11-20 17:53:36 +0100

    Fix for types, removed uneeded peer deps

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,9 @@
+import type { UNSAFE_RouteModules } from "react-router";
+declare global {
+	interface Window {
+		__reactRouterRouteModules: UNSAFE_RouteModules;
+	}
+}
 /**
  * Get the list of namespaces used by the application server-side so you could
  * set it on i18next init options.
@@ -9,24 +15,20 @@
  */
 export function getInitialNamespaces(): string[] {
 	let namespaces = Object.values(
-		// biome-ignore lint/suspicious/noExplicitAny: not sure where hte type definition comes from
-		(window as any).__reactRouterRouteModules,
-	).flatMap(
-		// biome-ignore lint/suspicious/noExplicitAny: making TS happy, not sure where the type definition comes from
-		(route: any) => {
-			if (typeof route?.handle !== "object") return [];
-			if (!route.handle) return [];
-			if (!("i18n" in route.handle)) return [];
-			if (typeof route.handle.i18n === "string") return [route.handle.i18n];
-			if (
-				Array.isArray(route.handle.i18n) &&
-				route.handle.i18n.every((value: unknown) => typeof value === "string")
-			) {
-				return route.handle.i18n as string[];
-			}
-			return [];
-		},
-	);
+		window.__reactRouterRouteModules,
+	).flatMap((route) => {
+		if (typeof route?.handle !== "object") return [];
+		if (!route.handle) return [];
+		if (!("i18n" in route.handle)) return [];
+		if (typeof route.handle.i18n === "string") return [route.handle.i18n];
+		if (
+			Array.isArray(route.handle.i18n) &&
+			route.handle.i18n.every((value: unknown) => typeof value === "string")
+		) {
+			return route.handle.i18n as string[];
+		}
+		return [];
+	});
 
 	return [...namespaces];
 }
