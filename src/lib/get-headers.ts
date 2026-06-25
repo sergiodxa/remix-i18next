b102680b8831cd58166dee44986ca551c874e684
Sergio Xalambrí <hello@sergiodxa.com>
2026-06-24 22:53:11 -0500

    Remove unused getHeaders function from the library

--- src/lib/get-headers.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Receives a Request or Headers objects.
- * If it's a Request returns the request.headers
- * If it's a Headers returns the object directly.
- */
-export function getHeaders(requestOrHeaders: Request | Headers): Headers {
-	if (requestOrHeaders instanceof Request) return requestOrHeaders.headers;
-	return requestOrHeaders;
-}
